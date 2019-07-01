f20501b1ea910fbded0745f96a7aa50721016a3f
Beryter <mg459046365@gmail.com>
2019-07-01 20:50:39 +0800

    添加栈相关数据
    实现一个栈，该栈带有出栈（pop）、入栈(push)、取最小元素（getMin）3个方法。保证这三个方法的时间复杂度都是O(1)

---
+++ DailySwiftAlgorithms/DailySwiftAlgorithms/Common/CustomError.swift
@@ -0,0 +1,13 @@
+//
+//  CustomError.swift
+//  DailySwiftAlgorithms
+//
+//  Created by Beryter on 2019/7/1.
+//  Copyright © 2019 Beryter. All rights reserved.
+//
+
+import Foundation
+
+enum CustomError: Error {
+    case stackIsEmpty
+}
---
+++ DailySwiftAlgorithms/DailySwiftAlgorithms/栈/Stack_20190701.swift
@@ -0,0 +1,49 @@
+//
+//  Stack_2019.7.1.swift
+//  DailySwiftAlgorithms
+//  实现一个栈，该栈带有出栈（pop）、入栈(push)、取最小元素（getMin）3个方法。保证这三个方法的时间复杂度都是O(1)
+//  Created by Beryter on 2019/7/1.
+//  Copyright © 2019 Beryter. All rights reserved.
+//
+
+import Foundation
+
+class FastGetMinStack {
+    /// 数据，实现栈
+    var stack = [Int]()
+    /// 辅助队列，存放最小数值,得到一个排序后的数组
+    var helperStack = [Int]()
+    
+    
+    /// 入栈
+    /// - Parameter element: 入栈元素
+    func push(_ element: Int) {
+        self.stack.append(element)
+        if self.helperStack.isEmpty || self.helperStack.last! >= element {
+            //注意临界值，栈中存在值相同的数据时
+            self.helperStack.append(element)
+        }
+    }
+    
+    
+    /// 出栈
+    func pop() throws -> Int {
+        guard self.stack.count > 0 else {
+            throw CustomError.stackIsEmpty
+        }
+        let val = self.stack.removeLast()
+        if self.helperStack.last! == val {
+            self.helperStack.removeLast()
+        }
+        return val
+    }
+    
+    /// 获取栈中最小值
+    func getMin() throws -> Int {
+        guard self.stack.count > 0 else {
+            throw CustomError.stackIsEmpty
+        }
+        return self.helperStack.last!
+    }
+    
+}
