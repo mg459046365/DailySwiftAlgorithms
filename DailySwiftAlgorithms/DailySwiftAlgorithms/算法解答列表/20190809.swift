e5716f378c395afb248f0e6f62465baf834651a5
Beryter <mg459046365@gmail.com>
2019-08-09 12:18:38 +0800

    更新算法列表
    一个无序数组里有若干个正整数，范围是1-100，其中99个整数都出现了偶数次，只有一个整数出现了奇数次，如何找到这个出现奇数次的整数。

--- DailySwiftAlgorithms/DailySwiftAlgorithms/算法解答列表/20190809.swift
+++ DailySwiftAlgorithms/DailySwiftAlgorithms/算法解答列表/20190809.swift
@@ -62,6 +62,25 @@ struct Helper_20190809 {
         return num
     }
     
+    
+    //MARK: - 一个无序数组里有若干个正整数，范围是1-100，其中99个整数都出现了偶数次，只有一个整数出现了奇数次，如何找到这个出现奇数次的整数。
+    
+    ///求奇数次的整数
+    ///
+    /// 思路核心思想： 异或运算。相同位得0，不同位得1。
+    /// 遍历整个数组，依次异或运算，由于异或运算在进行位运算时，相同为0，不同为1，因此所有出现偶数次的整数都会相互抵消变成0，只有唯一出现奇数次的整数
+    /// 会被留下
+    ///  时间复杂度为0(n)，空间复杂度为O(1)
+    /// - Parameter list: 整数列表
+    static func findOddTimeNumber(_ list: [Int]) -> Int {
+        var num = list[0]
+        for i in 1 ..< list.count {
+            num = num ^ list[i]
+        }
+        return num
+    }
+    
+    
     /// 测试方法
     static func test() {
         var list = [Int]()
@@ -76,8 +95,13 @@ struct Helper_20190809 {
         let res1 = findMissNumber1(list)
         let res2 = findMissNumber2(list)
         let res3 = findMissNumber3(list)
-        print("算法1得到的结果=\(res1)")
-        print("算法2得到的结果=\(res2)")
-        print("算法3得到的结果=\(res3)")
+        print("算法1得到的结果：\(res1)")
+        print("算法2得到的结果：\(res2)")
+        print("算法3得到的结果：\(res3)")
+        
+        
+        list = [3, 1, 3, 2, 4, 1, 4]
+        let res4 = findOddTimeNumber(list)
+        print("出现奇数次的数为：\(res4)")
     }
 }
