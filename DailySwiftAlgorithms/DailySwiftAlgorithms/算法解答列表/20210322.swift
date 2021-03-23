78458344450dec15fa01cf817c976f3abfb43575
Beryter <mg459046365@gmail.com>
2021-03-23 21:31:16 +0800

    添加算法
    找到所有数组中消失的数字

--- DailySwiftAlgorithms/DailySwiftAlgorithms/算法解答列表/20210322.swift
+++ DailySwiftAlgorithms/DailySwiftAlgorithms/算法解答列表/20210322.swift
@@ -31,7 +31,7 @@ import Foundation
 // 两个字符串都只由小写字符组成。
 
 class Solution_20210322 {
-    func isSubsequence(_ s: String, _ t: String) -> Bool {
+    class func isSubsequence(_ s: String, _ t: String) -> Bool {
         if s.isEmpty, t.isEmpty { return true }
         if s.count > t.count { return false }
         var tmp = t
@@ -47,5 +47,35 @@ class Solution_20210322 {
             tmp = String(tmp[tmpIdx...])
         }
         return true
+    }
+
+    // 给定一个范围在  1 ≤ a[i] ≤ n ( n = 数组大小 ) 的 整型数组，数组中的元素一些出现了两次，另一些只出现一次。
+    //
+    // 找到所有在 [1, n] 范围之间没有出现在数组中的数字。
+    //
+    // 您能在不使用额外空间且时间复杂度为O(n)的情况下完成这个任务吗? 你可以假定返回的数组不算在额外空间内。
+    //
+    // 示例:
+    //
+    // 输入:
+    // [4,3,2,7,8,2,3,1]
+    //
+    // 输出:
+    // [5,6]
+
+    class func findDisappearedNumbers(_ nums: inout [Int]) -> [Int] {
+        if nums.isEmpty { return [] }
+        var res = [Int]()
+        for i in 0 ..< nums.count {
+            if nums[abs(nums[i]) - 1] > 0 {
+                nums[abs(nums[i]) - 1] = -nums[abs(nums[i]) - 1]
+            }
+        }
+        for i in 0 ..< nums.count {
+            if nums[i] > 0 {
+                res.append(i + 1)
+            }
+        }
+        return res
     }
 }
